94baf5756b4660c731427939530910bae730c645
sebastian <sebvargas@gmail.com>
2023-01-05 10:57:47 -0800

    remove .vscode folder from git

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+# import blueprints
+from 
+
+def foo(a, b):
+    """
+    Returns the addition of two numbers.
+
+    :param a: Required first number.
+    :param b: Required second number.
+
+    :type a: int
+    :type b: int
+    :return: a + b.
+    :rtype: int
+
+    """
+    return a + b
+
+
+if __name__ == "__main__":
+    pass
